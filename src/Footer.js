72f4a75ac30c3e76cca1039f483ea70aaf24175c
Daniil <daniilk11@mail.ru>
2024-08-29 11:09:25 +0200

    add logo

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
+import bubenLogo from './public/buben_logo.svg';
 const Footer = () => {
     return (
         <footer className="bg-gradient-to-r from-green-50 to-green-100 shadow-md mt-auto">
@@ -8,29 +7,31 @@ const Footer = () => {
                 <div className="flex flex-col md:flex-row justify-between items-center">
                     <div className="mb-4 md:mb-0">
                         <span className="text-xl font-bold text-green-800">Buben Club</span>
-                        {/*//TODO add logo*/}
+                        <img src={bubenLogo} alt="Buben Club Logo" className="w-8 h-8 inline-block"/>
                     </div>
                     <div className="flex flex-col md:flex-row md:space-x-8">
                         <div className="mb-4 md:mb-0">
                             <h3 className="font-semibold text-green-800 mb-2">Help</h3>
                             <ul className="space-y-1">
                                 <li>
-                                    <Link to="/wiki" className="text-green-600 hover:text-green-800">How to become a member</Link>
-                                {/*https://wiki.buk.cvut.cz/en/for-members/faq*/}
-                                </li><li>
-                                    <Link to="/wiki" className="text-green-600 hover:text-green-800">Wiki</Link>
-                                {/*https://wiki.buk.cvut.cz/cs/home*/}
+                                    <a href="https://wiki.buk.cvut.cz/en/for-members/faq" target="_blank"
+                                       rel="noopener noreferrer"
+                                       className="text-green-600 hover:text-green-800">How to become a
+                                        member</a>
                                 </li>
                                 <li>
-                                    <Link to="/faq" className="text-green-600 hover:text-green-800">FAQ</Link>
-                                {/*  https://wiki.buk.cvut.cz/cs/for-members/faq  */}
+                                    <a href="https://wiki.buk.cvut.cz/cs/home" target="_blank" rel="noopener noreferrer"
+                                       className="text-green-600 hover:text-green-800">Wiki</a>
                                 </li>
                                 <li>
-                                    {/*<Link to="/about-reservation" className="text-green-600 hover:text-green-800">About*/}
-                                    {/*    Reservation</Link>*/}
                                     <a href="https://wiki.buk.cvut.cz/cs/club-zone/club-room" target="_blank"
                                        rel="noopener noreferrer"
-                                       className="text-green-600 hover:text-green-800">Instagram</a>
+                                       className="text-green-600 hover:text-green-800">FAQ About Reservation</a>
+                                </li>
+                                <li>
+                                    <a href="https://linktr.ee/bubenklub" target="_blank"
+                                       rel="noopener noreferrer"
+                                       className="text-green-600 hover:text-green-800">Links</a>
                                 </li>
                             </ul>
                         </div>
@@ -38,13 +39,17 @@ const Footer = () => {
                             <h3 className="font-semibold text-green-800 mb-2">Our social networks</h3>
                             <ul className="space-y-1">
                                 <li>
-                                    <a href="https://www.instagram.com/bubenklub/" target="_blank" rel="noopener noreferrer" className="text-green-600 hover:text-green-800">Instagram</a>
-                                </li>
-                                <li> https://discord.gg/eae5fAsz
-                                    <a href="https://discord.gg/cwb8mRD3Qg" target="_blank" rel="noopener noreferrer" className="text-green-600 hover:text-green-800">Discord</a>
+                                    <a href="https://www.instagram.com/bubenklub/" target="_blank"
+                                       rel="noopener noreferrer"
+                                       className="text-green-600 hover:text-green-800">Instagram</a>
                                 </li>
                                 <li>
-                                    <a href="https://t.me/+7HvQ_-rPlSxlZDcy" target="_blank" rel="noopener noreferrer" className="text-green-600 hover:text-green-800">Telegram</a>
+                                    <a href="https://discord.gg/cwb8mRD3Qg" target="_blank" rel="noopener noreferrer"
+                                       className="text-green-600 hover:text-green-800">Discord</a>
+                                </li>
+                                <li>
+                                    <a href="https://t.me/+7HvQ_-rPlSxlZDcy" target="_blank" rel="noopener noreferrer"
+                                       className="text-green-600 hover:text-green-800">Telegram</a>
                                 </li>
                             </ul>
                         </div>

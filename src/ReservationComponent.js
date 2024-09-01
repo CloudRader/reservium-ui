ccb20cf640f3675b673ea504311a0d2aede845bf
Daniil <daniilk11@mail.ru>
2024-09-01 13:03:25 +0200

    fix

--- src/ReservationComponent.js
+++ src/ReservationComponent.js
@@ -47,7 +47,73 @@ const ReservationComponent = ({ isLoggedIn, onLogout, roomCalendarLinks, service
 
     const formFields = useMemo(() => {
         const baseFields = [
-            // ... (previous fields remain unchanged)
+            {
+                name: 'startDate',
+                type: 'date',
+                labelText: 'Start Date',
+                labelColor: 'text-success',
+                defaultValue: getTomorrowDate(),
+                validation: (value) => {
+                    const year = new Date(value).getFullYear();
+                    return year > 2023 && year < 3000;
+                }
+            },
+            {
+                name: 'startTime',
+                type: 'time',
+                labelText: 'Start Time',
+                labelColor: 'text-success',
+                defaultValue: '17:00',
+                validation: (value) => /^([01]\d|2[0-3]):([0-5]\d)$/.test(value),
+            },
+            {
+                name: 'endDate',
+                type: 'date',
+                labelText: 'End Date',
+                labelColor: 'text-success',
+                defaultValue: getTomorrowDate(),
+                validation: (value) => {
+                    const year = new Date(value).getFullYear();
+                    return year > 2023 && year < 3000;
+                }
+            },
+            {
+                name: 'endTime',
+                type: 'time',
+                labelText: 'End Time',
+                labelColor: 'text-success',
+                defaultValue: '20:00',
+                validation: (value) => /^([01]\d|2[0-3]):([0-5]\d)$/.test(value),
+            },
+            {
+                name: 'purpose',
+                type: 'text',
+                labelText: 'Purpose',
+                labelColor: 'text-success',
+                validation: (value) => !!value,
+            },
+            {
+                name: 'guests',
+                type: 'number',
+                labelText: 'Number of Guests',
+                labelColor: 'text-success',
+                validation: (value) => value > 0 && value < 40,
+            },
+            {
+                name: 'email',
+                type: 'email',
+                labelText: 'Email',
+                labelColor: 'text-primary',
+                validation: (value) => /\S+@\S+\.\S+/.test(value),
+            },
+            {
+                name: 'type',
+                type: 'select',
+                labelText: 'Type of Reservation',
+                labelColor: 'text-primary',
+                options: reservationTypes,
+                validation: (value) => !!value
+            },
         ];
 
         if (additionalServices.length > 0) {

0a292778f9c3c29b2c76cd5ad2502d19e1d984db
Daniil <daniilk11@mail.ru>
2024-09-02 17:25:42 +0200

    fix success page

--- src/SuccesPage.js
+++ src/SuccesPage.js
@@ -4,33 +4,59 @@ import ServicesSection from './ServicesSection';
 
 const NotFoundPage = () => {
     const location = useLocation();
-    const { state } = location; // Access the state passed via navigation
+    const {state} = location; // Access the state passed via navigation
+
+    const response = JSON.parse(serverResponse);
+    // Check if the message matches the specific condition
+    const isTooManyPeopleMessage = response.message.includes("Too many people!");
+    const link = "https://wiki.buk.cvut.cz/cs/club-zone/club-room";
 
     return (
         <div className="min-h-screen bg-gradient-to-r from-green-50 to-green-100">
-            <div className="container mx-auto px-4 py-8 md:py-16 lg:py-52 flex flex-col md:flex-row items-stretch justify-between">
-                <div className="w-full md:w-1/2 lg:w-5/12 mb-8 md:mb-0 md:flex md:flex-col">
-                    <div className="bg-white p-6 md:p-8 text-center flex-grow flex flex-col justify-center">
-                        {/*<h1 className="text-4xl font-bold text-green-800 mb-4">Reservation made successfully</h1>*/}
-                        {/*<h2 className="text-2xl font-semibold text-green-700 mb-6">Reservation made successfully</h2>*/}
-                        {/*<p className="text-green-700 mb-8">Now you can see it on the calendar.</p>*/}
-                        {/*<p className="text-green-700 mb-8">Please read the terms of use for the spaces on our wiki.</p>*/}
-                        {/*<div>*/}
-                        {/*<a href="https://wiki.buk.cvut.cz/cs/club-zone/club-room" target="_blank"*/}
-                        {/*   rel="noopener noreferrer"*/}
-                        {/*   className="inline-flex no-underline items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500" */}
-                        {/*>WIKI</a>*/}
-                        {/*</div>*/}
-                        <p>Data received:</p>
-                        <pre>{JSON.stringify(state, null, 2)}</pre>
+            <div
+                className="container mx-auto px-4 py-8 md:py-16 lg:py-52 flex flex-col md:flex-row items-stretch justify-between">
+                {isTooManyPeopleMessage ? (
+                    <div className="w-full md:w-1/2 lg:w-5/12 mb-8 md:mb-0 md:flex md:flex-col">
+                        Now reservation is waiting for approval. For this type of reservation (more than 10 people) you
+                        will need to get additional confirmation. More information here:
+                        <a href="https://wiki.buk.cvut.cz/cs/club-zone/club-room" target="_blank"
+                           rel="noopener noreferrer">WIKI</a>
+                        . Server response {response}
                     </div>
-                </div>
-                <div className="w-full md:w-1/2 lg:w-7/12 md:flex md:flex-col">
-                    <ServicesSection/>
-                </div>
+                ) : (
+                    <div>
+                        <div className="w-full md:w-1/2 lg:w-5/12 mb-8 md:mb-0 md:flex md:flex-col">
+                            <div className="bg-white p-6 md:p-8 text-center flex-grow flex flex-col justify-center">
+                                <h1 className="text-4xl font-bold text-green-800 mb-4">Reservation made
+                                    successfully</h1>
+                                <h2 className="text-2xl font-semibold text-green-700 mb-6">Reservation made
+                                    successfully</h2>
+                                <p className="text-green-700 mb-8">Now you can see it on the calendar.</p>
+                                <p className="text-green-700 mb-8">Please read the terms of use for the spaces on our
+                                    wiki.</p>
+                                <div>
+                                    <a href="https://wiki.buk.cvut.cz/cs/club-zone/club-room" target="_blank"
+                                       rel="noopener noreferrer"
+                                       className="inline-flex no-underline items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+                                    >WIKI</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-full md:w-1/2 lg:w-7/12 md:flex md:flex-col">
+                            <ServicesSection/>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
 };
 
 export default NotFoundPage;
+
+<div className="container mx-auto px-4 py-8 md:py-16 lg:py-52 flex flex-col md:flex-row items-stretch justify-between">
+    <div className="w-full md:w-1/2 lg:w-5/12 mb-8 md:mb-0 md:flex md:flex-col">
+        Now reservation is waiting for approval. For this type of reservation (more than 10 people) you will need to get
+        additional confirmation. More information here: {link}. Server response {serverResponse}
+    </div>
+</div>

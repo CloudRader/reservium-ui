276affcf3c00cca800c2ef67b636fc1d3e7cf384
Daniil <daniilk11@mail.ru>
2024-09-04 13:04:56 +0200

    refactor

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,5 @@
 const config = {
-    serverURL: "https://reservation.buk.cvut.cz/api/",
+    serverURL: "https://reservation.buk.cvut.cz/api",
     googleCalendarApiKey:  "AIzaSyARjUqLIMYShXY_JQAuUYdzb3G4Malsv3c",
 };
 export default config;

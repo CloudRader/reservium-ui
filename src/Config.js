4827c5a0dcf98379d259621c8cd2cec41c119929
Daniil <daniilk11@mail.ru>
2024-09-04 17:18:47 +0200

    fix

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,5 @@
 const config = {
-    serverURL: "https://reservation.buk.cvut.cz",
+    serverURL: "https://api.reservation.buk.cvut.cz",
     googleCalendarApiKey:  "AIzaSyARjUqLIMYShXY_JQAuUYdzb3G4Malsv3c",
 };
 export default config;

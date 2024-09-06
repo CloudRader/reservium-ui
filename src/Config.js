ae638f6210dfc499b94aaba8ad7d9d00df05648e
Daniil <daniilk11@mail.ru>
2024-09-06 16:26:03 +0200

    fix mails

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,10 @@
 const config = {
     serverURL: "https://api.reservation.buk.cvut.cz",
     googleCalendarApiKey:  "AIzaSyARjUqLIMYShXY_JQAuUYdzb3G4Malsv3c",
+    reservationFormLink :  "https://drive.google.com/file/d/1GeEh4cKlcBzspV-AzsG_ag_WfVVgXl1X/view?usp=sharing",
+    reservationInfoLink :  "https://wiki.buk.cvut.cz/en/club-zone/club-room#reservation-system",
+    wikiInfoLink :  "https://wiki.buk.cvut.cz/en/club-zone/",
+    managerEmail: "artem.kuznetsov@buk.cvut.cz",
+    headOfDormitoryEmail: "Dana.Mencikova@cvut.cz"
 };
 export default config;

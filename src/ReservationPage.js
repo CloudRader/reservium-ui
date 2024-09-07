ea1cf1b628f362e4e28f8c4948c76b256ab8d9a9
Daniil <daniilk11@mail.ru>
2024-09-07 12:36:49 +0200

    fix load

--- src/ReservationPage.js
+++ src/ReservationPage.js
@@ -146,6 +146,10 @@ const ReservationPage = ({ isLoggedIn, onLogout, roomCalendarLinks, service }) =
         return <Logout onLogout={onLogout} />;
     }
 
+    if (!service) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
             <WarningMessage contact_mail={service.contact_mail}/>

444acbf4c6d30d8c2d3d89f4e428b4af0d7d95b9
Daniil <daniilk11@mail.ru>
2024-09-18 22:46:26 +0200

    add hook reformat code

--- src/App.js
+++ src/App.js
@@ -1,106 +1,55 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {Route, Routes} from 'react-router-dom';
+import {QueryClient, QueryClientProvider} from 'react-query';
+import axios from 'axios';
+
 import Header from './Header';
 import ReservationPage from "./ReservationPage";
-import config from "./Config";
-import CreateNewCalendar from "./CreateNewCalendar";
-import CreateNewMiniService from "./CreateNewMiniService";
+// import CreateNewCalendar from "./CreateNewCalendar";
+// import CreateNewMiniService from "./CreateNewMiniService";
 import {Login, useAuth} from "./Login";
 import Logout from "./Logout";
-import axios from 'axios';
 import Footer from "./Footer";
 import NotFoundPage from "./NotFoundPage";
 import LoginHandler from "./LoginHandler";
 import SuccessPage from "./SuccessPage";
-import { QueryClient, QueryClientProvider } from 'react-query';
+import {useReservationData} from './hooks/useReservationData';
+
 axios.defaults.withCredentials = true;
 
 const queryClient = new QueryClient();
 
-async function getReservationServiceData() {
-    try {
-        const response = await axios.get(`${config.serverURL}/reservation_services/`);
-        const data = response.data;
-
-        const services = data.map(info => ({
-            wikiLink: info.web,
-            linkName: info.alias,
-            serviceName: info.name,
-            contact_mail: info.contact_mail,
-            public: info.public,
-            reservation_types: info.calendars.map(calendar => calendar.reservation_type),
-            calendarIds: info.calendars.reduce((acc, calendar) => {
-                acc[calendar.reservation_type] = calendar.id;
-                return acc;
-            }, {})
-        }));
-
-        const calendars = data.reduce((acc, info) => {
-            acc[`${info.alias}`] =  info.calendars.map((calendar) => {
-                const calAcc = {};
-                calAcc["googleCalendarId"] = calendar.id;
-                calAcc["className"] = calendar.reservation_type;
-                calAcc["backgroundColor"] = calendar.color;
-                calAcc["borderColor"] = calendar.color;
-                return calAcc;
-            }, {});
-
-            return acc;
-        }, {});
-
-        return {
-            services: services,
-            calendars: calendars
-        };
-    } catch (error) {
-        console.log('Error fetching reservation service data:', error);
-        return {
-            services: [],
-            calendars: {}
-        };
-    }
-}
-
-function App() {
+function AppContent() {
     const {isLoggedIn, username, userRoles, logout} = useAuth();
-    const [services, setServices] = useState([]);
-    const [calendars, setCalendars] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+    const {data, isLoading, isError} = useReservationData(isLoggedIn);
 
-    useEffect(() => {
-        async function fetchData() {
-            const {services, calendars} = await getReservationServiceData();
-            setServices(services);
-            setCalendars(calendars);
-            setIsLoading(false);
-        }
+    if (isError) {
+        return <div>Error loading data. Please try again later.</div>;
+    }
 
-        if (isLoggedIn) fetchData();
-
-    }, [isLoggedIn]);
+    const {services, calendars} = data || {services: [], calendars: {}};
 
 
-    if (isLoading && isLoggedIn) { // change logic here TodO
+    if (isLoading) {
         return <div>Loading...</div>; // or a more sophisticated loading component
     }
 
     return (
-        <div>
-            <Header isLoggedIn={isLoggedIn} username={username} userRoles={userRoles} onLogout={logout}
+        <>
+            <Header isLoggedIn={isLoggedIn} username={username} userRoles={userRoles}
+                    onLogout={logout}
                     services={services}/>
-            <QueryClientProvider client={queryClient}>
-            <Routes>
-                {/*when login go to back-end redirect to IS then redirect to logined(with needed credentials) */}
-                <Route path='/login' element={<LoginHandler/>}/>
-                {/* send it to back-end for session get data from back and make components*/}
-                <Route path='/logined' element={<Login/>}/>
-                {/*then go here as default page*/}
-                <Route key='/' path='/' element={<ReservationPage isLoggedIn={isLoggedIn} onLogout={logout}
-                                                               roomCalendarLinks={calendars["club"]}
-                                                               service={services[0]}/>}/>
+                <Routes>
+                    {/*when login go to back-end redirect to IS then redirect to logined(with needed credentials) */}
+                    <Route path='/login' element={<LoginHandler/>}/>
+                    {/* send it to back-end for session get data from back and make components*/}
+                    <Route path='/logined' element={<Login/>}/>
+                    {/*then go here as default page*/}
+                    <Route key='/' path='/' element={<ReservationPage isLoggedIn={isLoggedIn} onLogout={logout}
+                                                                      roomCalendarLinks={calendars["club"]}
+                                                                      service={services[0]}/>}/>
 
-                {services
-                    .map(service => (
+                    {services.map(service => (
                         <Route
                             key={service.linkName}
                             path={`/${service.linkName}`}
@@ -115,23 +64,36 @@ function App() {
                         />
                     ))}
 
-                <Route path='/logout' element={<Logout onLogout={logout}/>}/>
-                <Route path="*" element={<NotFoundPage/>}/>
+                    <Route path='/logout' element={<Logout onLogout={logout}/>}/>
+                    <Route path="*" element={<NotFoundPage/>}/>
 
-                <Route path="/success" element={<SuccessPage />}/>
+                    <Route path="/success" element={<SuccessPage/>}/>
 
-                {/*<Route path='/' element={<HomePage />} />*/}
+                    {/*<Route path='/' element={<HomePage />} />*/}
 
-                {/*// if section role === "manager" show this */}
-                <Route path='/create-new-calendar'
-                       element={<CreateNewCalendar isLoggedIn={isLoggedIn} username={username}/>}/>
-                <Route path='/create-new-miniservice'
-                       element={<CreateNewMiniService isLoggedIn={isLoggedIn} username={username}/>}/>
-            </Routes>
-            </QueryClientProvider>
+                    {/*{userRoles.includes("manager") && (*/}
+                    {/*    <>*/}
+                    {/*        <Route*/}
+                    {/*            path='/create-new-calendar'*/}
+                    {/*            element={<CreateNewCalendar isLoggedIn={isLoggedIn} username={username} />}*/}
+                    {/*        />*/}
+                    {/*        <Route*/}
+                    {/*            path='/create-new-miniservice'*/}
+                    {/*            element={<CreateNewMiniService isLoggedIn={isLoggedIn} username={username} />}*/}
+                    {/*        />*/}
+                    {/*    </>*/}
+                    {/*)}*/}
+                </Routes>
             <Footer/>
-        </div>
+        </>
     );
 }
 
+function App() {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <AppContent />
+        </QueryClientProvider>
+    );
+}
 export default App;
---
+++ src/hooks/useReservationData.js
@@ -0,0 +1,86 @@
+import {useQuery} from 'react-query';
+import axios from 'axios';
+import config from "../Config";
+
+axios.defaults.withCredentials = true;
+async function fetchReservationData() {
+    const response = await axios.get(`${config.serverURL}/reservation_services/`);
+    const data = response.data;
+
+    const services = data.map(info => ({
+        wikiLink: info.web,
+        linkName: info.alias,
+        serviceName: info.name,
+        contact_mail: info.contact_mail,
+        public: info.public,
+        reservation_types: info.calendars.map(calendar => calendar.reservation_type),
+        calendarIds: info.calendars.reduce((acc, calendar) => {
+            acc[calendar.reservation_type] = calendar.id;
+            return acc;
+        }, {})
+    }));
+
+    const calendars = data.reduce((acc, info) => {
+        acc[`${info.alias}`] = info.calendars.map((calendar) => ({
+            googleCalendarId: calendar.id,
+            className: calendar.reservation_type,
+            backgroundColor: calendar.color,
+            borderColor: calendar.color,
+        }));
+        return acc;
+    }, {});
+
+    return { services, calendars };
+}
+
+export function useReservationData(isLoggedIn) {
+    return useQuery('reservationData', fetchReservationData, {
+        enabled: isLoggedIn,
+        staleTime: Infinity, // This data doesn't change often, so we can cache it indefinitely
+        cacheTime: 1000 * 60 * 60, // Cache for 1 hour
+    });
+}
+
+// async function getReservationServiceData() {
+//     try {
+//         const response = await axios.get(`${config.serverURL}/reservation_services/`);
+//         const data = response.data;
+//
+//         const services = data.map(info => ({
+//             wikiLink: info.web,
+//             linkName: info.alias,
+//             serviceName: info.name,
+//             contact_mail: info.contact_mail,
+//             public: info.public,
+//             reservation_types: info.calendars.map(calendar => calendar.reservation_type),
+//             calendarIds: info.calendars.reduce((acc, calendar) => {
+//                 acc[calendar.reservation_type] = calendar.id;
+//                 return acc;
+//             }, {})
+//         }));
+//
+//         const calendars = data.reduce((acc, info) => {
+//             acc[`${info.alias}`] = info.calendars.map((calendar) => {
+//                 const calAcc = {};
+//                 calAcc["googleCalendarId"] = calendar.id;
+//                 calAcc["className"] = calendar.reservation_type;
+//                 calAcc["backgroundColor"] = calendar.color;
+//                 calAcc["borderColor"] = calendar.color;
+//                 return calAcc;
+//             }, {});
+//
+//             return acc;
+//         }, {});
+//
+//         return {
+//             services: services,
+//             calendars: calendars
+//         };
+//     } catch (error) {
+//         console.log('Error fetching reservation service data:', error);
+//         return {
+//             services: [],
+//             calendars: {}
+//         };
+//     }
+// }

c0579f49e46132f15a456fe8d8c940b495639749
Daniil <daniilk11@mail.ru>
2024-09-24 13:29:24 +0200

    fix

--- src/LoginToIS.js
+++ src/LoginToIS.js
@@ -1,8 +1,8 @@
-import { useEffect } from 'react';
+import {useEffect} from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import config from "./Config";
-axios.defaults.withCredentials = true;
+
 
 /**
  * Gets the login URL from the server and redirects the user to it.
@@ -13,27 +13,14 @@ const LoginToIS = () => {
     useEffect(async () => {
         const fetchLoginUrl = async () => {
             try {
+                axios.defaults.withCredentials = true;
                 const response = await axios.get(`${config.serverURL}/users/login`);
-                const loginUrl = response.data;
+                window.location.href = response.data;
 
-                const loginWindow = window.open(loginUrl, '_blank', 'noopener,noreferrer');
-
-                if (loginWindow) {
-                    // // Poll to check if the login window is closed
-                    // const checkWindowClosed = setInterval(() => {
-                    //     if (loginWindow.closed) {
-                    //         clearInterval(checkWindowClosed);
-                    //         navigate('/logined');
-                    //     }
-                    // }, 1000);
-                } else {
-                    // If window.open fails, fall back to redirection
-                    window.location.href = loginUrl;
-                }
             } catch (error) {
                 // alert('Error fetching login URL:' + error);
                 console.error('Error fetching login URL:', error);
-                // navigate('/');
+                navigate('/');
             }
         };
         await fetchLoginUrl();

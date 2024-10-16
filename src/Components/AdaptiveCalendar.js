2d1031c970bbb03567a2360e593de1aaf15b1a20
Daniil <daniilk11@mail.ru>
2024-10-16 21:02:03 +0200

    refactor

--- src/Components/AdaptiveCalendar.js
+++ src/Components/AdaptiveCalendar.js
@@ -1,4 +1,4 @@
-import React, {memo, useCallback, useEffect, useState} from "react";
+import React, {memo, useCallback, useState} from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
--- src/Constants.js
+++ src/Constants.js
@@ -1,10 +1,10 @@
 const constants = {
     serverURL: "https://api.reservation.buk.cvut.cz",
+    headOfDormitoryEmail: "renata.prihodova@cvut.cz",
     googleCalendarApiKey: process.env['REACT_APP_GOOGLE_CALENDAR_API_KEY'],
     reservationFormLink : "https://drive.google.com/file/d/1GeEh4cKlcBzspV-AzsG_ag_WfVVgXl1X/view?usp=sharing",
     reservationInfoLink : "https://wiki.buk.cvut.cz/en/club-zone/club-room#reservation-system",
     wikiInfoLink : "https://wiki.buk.cvut.cz/en/club-zone/",
-    headOfDormitoryEmail: "renata.prihodova@cvut.cz",
     contactMail: "provoz@buk.cvut.cz",
     feedBackEmail: "provoz@buk.cvut.cz",
     services: [
@@ -18,6 +18,10 @@ const constants = {
         { name: "Internet", url: "https://wiki.buk.cvut.cz/en/for-members/internet" },
         { name: "Events", url: "https://www.instagram.com/bubenklub/" }
     ],
+    // set hours in int format
+    default_reservation_start_time: 17,
+    default_reservation_end_time: 21,
+    MOBILE_SCREEN_BREAKPOINT: 1024,
 };
 
 export default constants;

1e74f4edfb9df328a673394a50f46416bdead464
Daniil <daniilk11@mail.ru>
2024-10-17 16:32:32 +0200

    refactor

--- src/Components/EditCalendars.js
+++ src/Components/EditCalendars.js
@@ -1,31 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom';
-import UniversalLayout from "../UniversalLayout";
+import React from 'react';
 import EditTable from "./EditTable";
 
 const EditCalendars = ({roomCalendarLinks, serviceName }) => {
-    // const [calendars, setCalendars] = useState([]);
-    // const { serviceId } = useParams();
-
-    // useEffect(() => {
-    //     // Fetch calendars data here
-    //     // For now, we'll use mock data
-    //     const mockCalendars = [
-    //         { id: '1', reservation_type: 'Type A',},
-    //         { id: '2', reservation_type: 'Type B', },
-    //         { id: '3', reservation_type: 'Type C', },
-    //     ];
-    //
-    //     setCalendars(mockCalendars);
-    // }, []);
 
     const mockcolumHeaders = [
         'Calendar Id',
         'className',
         'backgroundColor',
         'borderColor',
+        'Actions',
     ];
-
 
     return (
         <EditTable name={'Calendars'} data={roomCalendarLinks} columHeaders={mockcolumHeaders} editLink={`/edit-calendar/`} addLink={`/add-calendar/${serviceName}`} />

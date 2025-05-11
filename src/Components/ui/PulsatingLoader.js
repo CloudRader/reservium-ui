2bfbb947d7aaccaa9356f26643f0ac49e8308e26
Daniil <daniilk11@mail.ru>
2025-05-11 11:48:12 +0200

    refactor PulsatingLoader component to enhance visual presentation; update background and add a container for improved aesthetics and user experience

--- src/Components/ui/PulsatingLoader.js
+++ src/Components/ui/PulsatingLoader.js
@@ -2,39 +2,41 @@ import React from 'react';
 
 const PulsatingLoader = ({ color = "#10B981" }) => {
     return (
-        <div className="flex justify-center items-center h-full w-full fixed top-0 left-0 bg-green-100 bg-opacity-75 z-50">
-            <svg width="100" height="30" viewBox="0 0 120 30" xmlns="http://www.w3.org/2000/svg" fill={color}>
-                <circle cx="15" cy="15" r="15">
-                    <animate attributeName="r" from="15" to="15"
-                             begin="0s" dur="0.8s"
-                             values="15;9;15" calcMode="linear"
-                             repeatCount="indefinite" />
-                    <animate attributeName="fill-opacity" from="1" to="1"
-                             begin="0s" dur="0.8s"
-                             values="1;.5;1" calcMode="linear"
-                             repeatCount="indefinite" />
-                </circle>
-                <circle cx="60" cy="15" r="9" fillOpacity="0.3">
-                    <animate attributeName="r" from="9" to="9"
-                             begin="0s" dur="0.8s"
-                             values="9;15;9" calcMode="linear"
-                             repeatCount="indefinite" />
-                    <animate attributeName="fill-opacity" from="0.5" to="0.5"
-                             begin="0s" dur="0.8s"
-                             values=".5;1;.5" calcMode="linear"
-                             repeatCount="indefinite" />
-                </circle>
-                <circle cx="105" cy="15" r="15">
-                    <animate attributeName="r" from="15" to="15"
-                             begin="0s" dur="0.8s"
-                             values="15;9;15" calcMode="linear"
-                             repeatCount="indefinite" />
-                    <animate attributeName="fill-opacity" from="1" to="1"
-                             begin="0s" dur="0.8s"
-                             values="1;.5;1" calcMode="linear"
-                             repeatCount="indefinite" />
-                </circle>
-            </svg>
+        <div className="flex justify-center items-center h-full w-full fixed top-0 left-0 bg-white bg-opacity-50 backdrop-blur-sm z-50">
+            <div className="bg-white p-8 rounded-lg shadow-lg">
+                <svg width="100" height="30" viewBox="0 0 120 30" xmlns="http://www.w3.org/2000/svg" fill={color}>
+                    <circle cx="15" cy="15" r="15">
+                        <animate attributeName="r" from="15" to="15"
+                            begin="0s" dur="0.8s"
+                            values="15;9;15" calcMode="linear"
+                            repeatCount="indefinite" />
+                        <animate attributeName="fill-opacity" from="1" to="1"
+                            begin="0s" dur="0.8s"
+                            values="1;.5;1" calcMode="linear"
+                            repeatCount="indefinite" />
+                    </circle>
+                    <circle cx="60" cy="15" r="9" fillOpacity="0.3">
+                        <animate attributeName="r" from="9" to="9"
+                            begin="0s" dur="0.8s"
+                            values="9;15;9" calcMode="linear"
+                            repeatCount="indefinite" />
+                        <animate attributeName="fill-opacity" from="0.5" to="0.5"
+                            begin="0s" dur="0.8s"
+                            values=".5;1;.5" calcMode="linear"
+                            repeatCount="indefinite" />
+                    </circle>
+                    <circle cx="105" cy="15" r="15">
+                        <animate attributeName="r" from="15" to="15"
+                            begin="0s" dur="0.8s"
+                            values="15;9;15" calcMode="linear"
+                            repeatCount="indefinite" />
+                        <animate attributeName="fill-opacity" from="1" to="1"
+                            begin="0s" dur="0.8s"
+                            values="1;.5;1" calcMode="linear"
+                            repeatCount="indefinite" />
+                    </circle>
+                </svg>
+            </div>
         </div>
     );
 };

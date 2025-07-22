9b7b38e477160b95f74d2a552c1e96bd5fd131f6
Daniil <daniilk11@mail.ru>
2025-07-22 11:25:46 +0200

    remove unnecessary whitespace in tailwind.config.js and add positive response message to messages.js

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,4 +1,5 @@
 export const messages = {
     tooManyPeople: "Reservation exceeds allowed number of guests.",
     nightTime: "Reservation during night hours requires manager confirmation.",
+    positiveResponse: "Reservation was made successfully",
 };

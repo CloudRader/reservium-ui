b354349bce09324c70e419e15d8c7e47c57001f5
Daniil <daniilk11@mail.ru>
2026-01-12 20:29:04 +0100

    refactor: update Keycloak configuration to use environment variables

--- src/features/auth/components/Keycloak.jsx
+++ src/features/auth/components/Keycloak.jsx
@@ -1,9 +1,9 @@
-import Keycloak from "keycloak-js";
+import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-    url: "https://auth.buk.cvut.cz", // change to your Keycloak base URL
-    realm: "buk.cvut.cz",             // change to your realm
-    clientId: "reservation-frontend-app",      // change to your clientId
+  url: import.meta.env.VITE_KEYCLOAK_URL,
+  realm: import.meta.env.VITE_KEYCLOAK_REALM,
+  clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID,
 });
 
 export default keycloak;

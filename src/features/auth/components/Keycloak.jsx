8eb01642f5bb5cd2960a5dcc4d4d681acab2ba98
DarkRader <73800331+DarkRader@users.noreply.github.com>
2026-04-25 12:16:50 +0200

    change realm to dev

--- src/features/auth/components/Keycloak.jsx
+++ src/features/auth/components/Keycloak.jsx
@@ -2,7 +2,7 @@ import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
   url: 'https://auth.buk.cvut.cz', // change to your Keycloak base URL
-  realm: 'buk.cvut.cz', // change to your realm
+  realm: 'dev.cvut.cz', // change to your realm
   clientId: 'reservation-frontend-app', // change to your clientId
 });
 

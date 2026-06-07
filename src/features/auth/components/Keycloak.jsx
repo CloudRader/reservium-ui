4cfd7b5a3e5d5c630efbfb007c00914ea06ac737
DarkRader <73800331+DarkRader@users.noreply.github.com>
2026-06-07 16:26:16 +0200

    Change to prod buk.cvut.cz domain

--- src/features/auth/components/Keycloak.jsx
+++ src/features/auth/components/Keycloak.jsx
@@ -2,7 +2,7 @@ import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
   url: 'https://auth.buk.cvut.cz', // change to your Keycloak base URL
-  realm: 'dev.buk.cvut.cz', // change to your realm
+  realm: 'buk.cvut.cz', // change to your realm
   clientId: 'reservation-frontend-app', // change to your clientId
 });
 

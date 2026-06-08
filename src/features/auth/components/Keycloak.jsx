f414761e4ac98590fc541d75083edd4de5d6b633
Artem Kuznetsov <artyom.20century@gmail.com>
2026-06-08 17:12:19 +0200

    fix(build): add or return import.meta.env for local envs

--- src/features/auth/components/Keycloak.jsx
+++ src/features/auth/components/Keycloak.jsx
@@ -1,9 +1,9 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-  url: window._env_?.VITE_KEYCLOAK_URL,
-  realm: window._env_?.VITE_KEYCLOAK_REALM,
-  clientId: window._env_?.VITE_KEYCLOAK_CLIENT_ID,
+  url: window._env_?.VITE_KEYCLOAK_URL || import.meta.env.VITE_KEYCLOAK_URL,
+  realm: window._env_?.VITE_KEYCLOAK_REALM || import.meta.env.VITE_KEYCLOAK_REALM,
+  clientId: window._env_?.VITE_KEYCLOAK_CLIENT_ID || import.meta.env.VITE_KEYCLOAK_CLIENT_ID,
 });
 
 export default keycloak;
